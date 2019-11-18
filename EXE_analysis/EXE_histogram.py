96c5843326cc5a9934afa139deb268f2eda1442b
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2019-11-18 15:11:03 -0700

    Developed get_avg_weights and deprecated the internal method in get_equil_info. Improved multi-file processing (benchmarking) and fixed a bug in get_final_counts

--- EXE_analysis/EXE_histogram.py
+++ EXE_analysis/EXE_histogram.py
@@ -6,12 +6,13 @@ ensemble simulation. The script performs the following analysis:
 3. Show and save the histogram at the time that the weights are equilibrated.
 4. Print out equilibrated Wang-Landau weights which can directly be pasted to
     the .mdp file if needed.
-5. Print out the averaged Wang-Landau weights for a user-definied portion of
-    the last iteration.
+5. Print out the averaged Wang-Landau weights over a user-definied length of 
+    simulation.
 6. Estimate the uncertainty in free energy difference between the first and
     the last state from the final histogram.
 """
 
+import os
 import sys
 import glob
 import argparse
@@ -33,20 +34,24 @@ def initialize():
                     ensemble simulations.')
     parser.add_argument('-l',
                         '--log',
+                        type=str,
                         nargs='+',
                         help='The filename(s) of the log file. Wildcards \
                             available.')
-    parser.add_argument('-f',
-                        '--frac',
-                        help='The fraction which is used to calcuate the \
-                            average weights. A fraction of 0.25 (default) \
-                            means average the weights of the last 25 percent \
-                            of the steps before weights equilibration.')
     parser.add_argument('-k',
                         '--keyword',
+                        type=str,
                         nargs='+',
                         help='The keyword used in the filename of the figure \
                             produced.')
+    parser.add_argument('-a',
+                        '--avg_len',
+                        type=float,
+                        default=20,
+                        help='The length of the simulation that the calculation \
+                            of average weights are based on. -a 20 means that the \
+                            weights of last 20 ns before the weights are eauilibrated\
+                            will be averaged. Default: 20 ns.')
 
     args_parse = parser.parse_args()
 
@@ -69,14 +74,17 @@ class LogInfo:
         f.close()
 
         self.dt, self.cutoff, self.wl_scale, self.N_states, self.fixed, \
-            self.init_wl, self.temp, self.start = None, None, None, None, \
-            None, None, None, None
+            self.init_wl, self.temp, self.start, self.nstlog = None, None, \
+            None, None, None, None, None, None, None
         line_n = 0
 
         for l in lines:
             line_n += 1
             if 'dt  ' in l and self.dt is None:
                 self.dt = float(l.split('=')[1])
+
+            if 'nstlog' in l and self.nstlog is None:
+                self.nstlog = float(l.split('=')[1])
 
             if 'weight-equil-wl-delta' in l and self.cutoff is None:
                 self.cutoff = float(l.split('=')[1])
@@ -116,11 +124,13 @@ class EXEAnalysis(LogInfo):
         """
         self.equil = False  # for examining if weights are equilibrated
         self.equil_time = None
+        self.avg_start = None  # the starting piont of the weights average calculation
+        self.avg_end = None   # the endpoint time frame of weights average calculation
         LogInfo.__init__(self, logfile)
         # So any instance of EXEAnalysis will have all the properties that a
         # LogInfo instance has
 
-    def get_equil_info(self, logfile, frac):
+    def get_equil_info(self, logfile):
         """
         This function analyzes the log file and performs the following tasks:
         1. Output the data needed for plotting Wang-Landau incrementor as a
@@ -134,9 +144,6 @@ class EXEAnalysis(LogInfo):
         ----------
         logfile : str
             The filename of the log file
-        frac : float
-            The fraction of the time before the weights were equilibrated.
-            Used for calculating the average weights.
 
         Returns
         -------
@@ -146,9 +153,6 @@ class EXEAnalysis(LogInfo):
             The array of Wang-Landau incrementors
         final_weights : str
             The final equilibrated lambda weights
-        weights_avg :
-            The average weights of the last portion of steps before the
-            weights are equilibrated
         equil_time : float
             The time that the weights are equilibrated
         """
@@ -161,9 +165,6 @@ class EXEAnalysis(LogInfo):
         wl_incrementor = []
         step = []
         line_n = self.start   # start counting from self.start
-        weights = []          # a list of weights at a certain step
-        weights_all = []      # a list of lists of weights at different time
-        # steps
 
         # ============= 2. Extract wl-incrementors and weights ===============
         for l in lines[self.start:]:  # skip the metadata
@@ -186,16 +187,7 @@ class EXEAnalysis(LogInfo):
                     if 'Wang-Landau incrementor is:' in l_search:
                         wl_incrementor.append(float(l_search.split(':')[1]))
 
-            # Create the list to calculate the average weights (first find the
-            # point of the last update before the weights are equilibrated)
-            if 'Wang-Landau incrementor is' in l and len(wl_incrementor) > 0 \
-                    and (wl_incrementor[-1] > self.cutoff) \
-                    and (wl_incrementor[-1] * self.wl_scale <= self.cutoff):
-                for i in range(self.N_states):
-                    weights.append(float(lines[line_n + i + 1].split()[6]))
-                weights_all.append(weights)
-                weights = []
-
+            # Collect revelant information about weights equilibration
             if 'Weights have equilibrated' in l:
                 self.equil = True
 
@@ -233,12 +225,14 @@ class EXEAnalysis(LogInfo):
                                                       split()[5]))
 
                 wl_incrementor = np.array(wl_incrementor)
-                time = np.array(step) * self.dt
-                self.equil_time = float(equil_step) * self.dt / 1000
+                time = np.array(step) * self.dt  # time array for plotting
+                self.equil_time = float(equil_step) * self.dt / 1000  # units: ns
+
+                # Note: avg_end will not be taken into account in the average calculation
+                avg_endstep = int(equil_step) - (int(equil_step) % self.nstlog) + self.nstlog
+                self.avg_end = avg_endstep * self.dt   # units: ps
                 
                 break
-
-
 
         # ========== 3. Exit if the weights have not equilibrated ============
         if self.equil is False:
@@ -265,24 +259,66 @@ class EXEAnalysis(LogInfo):
               % err_kt)
         print('Or at the simulation temperature (%s K), the uncertainty is %5.3f kcal/mol\n' % (str(float(self.temp)), err_kcal))
 
-        # ================ 5. Average weights calculation ====================
-        n_points = np.floor(frac * len(weights_all))
-        # number of lists to be averaged
+        return time, wl_incrementor, final_weights, equil_counts
 
-        weights_all = np.array(weights_all[-int(n_points):])
-        # only average a last certain portion
+    def get_avg_weights(self, logfile, avg_len):
+        """
+        This function performs the weights average calculation.
 
+        Parameters
+        ----------
+        logfile : str
+            The filename of the log file
+
+        Returns
+        -------
+        weights_avg : np.array
+            The average weights over an user-defined length of the simulation.
+        """
+        self.avg_start = self.avg_end - avg_len * 1000     # unit: ps
+        if self.avg_start <= 0:
+            print('The starting point of the weights average calculation is less than 0!')
+            print('Please specified a shorter length of simulation for the',
+                  'weights average calculation.')
+            sys.exit()
+
+        f = open(logfile, 'r')
+        lines = f.readlines()
+        f.close()
+
+        search_start = False
+        line_n = self.start
+        weights = []    # to store weights at each time frame to be averaged
+        weights_all = []    # a list of list of weights at different time frames
+
+        # collect the data of weights to be averaged
+        for l in lines[self.start:]:    # skip the metadata
+            line_n += 1
+            if str(self.avg_start) in l: 
+                search_start = True
+
+            if 'Wang-Landau incrementor is:' in l and search_start is True:
+                for i in range(self.N_states):
+                    weights.append(float(lines[line_n + i + 1].split()[6]))
+                weights_all.append(weights)
+                weights = []
+
+            if str(self.avg_end) in l:
+                break
+        
+        # Average the weights
         list_sum = 0
+        weights_all = np.array(weights_all)
         for i in range(len(weights_all)):
             list_sum += weights_all[i]
         weights_avg_float = list_sum/len(weights_all)
         weights_avg_float = [round(x, 5) for x in weights_avg_float]
 
-        weights_avg = ''    # make weights_ave as a string to be easily copied
+        weights_avg = ''   # make weights_avg as a string to be easily copied
         for i in range(len(weights_avg_float)):
             weights_avg += (' ' + str(weights_avg_float[i]))
 
-        return time, wl_incrementor, final_weights, weights_avg, equil_counts
+        return weights_avg
 
     def get_final_counts(self, logfile):
         """
@@ -306,17 +342,20 @@ class EXEAnalysis(LogInfo):
         f.close()
         lines.reverse()     # from this point, lines has been reverse
 
+        final_found = False
         line_n = 0
         final_counts = np.zeros(self.N_states)
         for l in lines:
             #  print(l)   # this will print from the bottom
             line_n += 1
-            if 'MC-lambda information' in l:
+            if 'MC-lambda information' in l:  # should find this line first
+                final_found = True
                 for i in range(self.N_states):
                     # start from lines[line_n - 3]
                     # 'MC-lambda information' is lines[line_n - 1]
                     final_counts[i] = float(lines[line_n - 3 - i].split()[5])
-            if '  Step  ' in l:
+
+            if '  Step  ' in l and final_found is True:
                 # '    Step      Time    ' is lines[line_n - 1]
                 final_time = float(lines[line_n - 2].split()[1])  # in ps
                 break
@@ -338,6 +377,16 @@ def main():
     rc('mathtext', **{'default': 'regular'})
     plt.rc('font', family='serif')
 
+    if args.log is None:
+        args.log = []
+        for file in os.listdir('.'):
+            if file.endswith('.log'):
+                args.log.append(file)
+        if not args.log:
+            print('No log files found! Please check if the directory is correct or specify the name of the log files.')
+        else:
+            args.log = natsort.natsorted(args.log)
+
     if isinstance(args.log, str):                # the case of only one input
         args.log = list(args.log)
     if isinstance(args.log, str) and '*' in args.log:  # to enables wildcards
@@ -348,25 +397,38 @@ def main():
     # Check if the default should be used
     if args.keyword is None:
         args.keyword = [logname.split('.')[0] for logname in args.log]
-    if args.frac is None:
-        args.frac = 0.25
-    elif float(args.frac) <= 0 or float(args.frac) >= 1:
-        print('Error: The fraction for average weights calculation should be \
-            between 0 and 1!')
-        sys.exit()
+
+    log_str = ''
+    for i in range(len(args.log)):
+        if i == 0:
+            log_str += args.log[i]         
+        elif i != len(args.log) - 1:
+            log_str += ', '
+            log_str += args.log[i]
+        else:
+            log_str += ', and '
+            log_str += args.log[i]
 
     print('\n')
+    print('The log files to be analyzed: %s.' % log_str)
+    print('Length of the simulation for the weights average calculation: %s ns.\n' % args.avg_len)
+    e0 = timer.time()
+    time_needed.append(e0 - s0)
+
     for i in range(len(args.log)):
+        s1 = timer.time()
         result_str = 'Data analysis of the file %s:' % args.log[i]
         print(result_str)
         print('=' * len(result_str))
 
         log_info = LogInfo(args.log[i])
         EXE = EXEAnalysis(args.log[i])
+        e1 = timer.time()
+        time_needed.append(e1 - s1)
 
         if log_info.fixed is False:
-            [time, wl_incrementor, weights_f, weights_a, equil_counts] = \
-                EXE.get_equil_info(args.log[i], args.frac)
+            s2 = timer.time()
+            [time, wl_incrementor, weights_f, equil_counts] = EXE.get_equil_info(args.log[i])
             # If the weights are not equilibrated, the code temrinates here.
 
             time = time / 1000  # convert from ps to ns
@@ -374,15 +436,13 @@ def main():
             # Print the results!
             print('The weights were equilibrated at %5.3f ns\n' %
                   EXE.equil_time)
-            print('The average weights of the last %s percent' %
-                  str(args.frac * 100), 'of steps right before the weights ' 
-                  'are equilibrated are: ', weights_a, '\n')
-            print('The final weights are: ', weights_f)
-            e0 = timer.time()
-            time_needed.append(e0 - s0)
+            print('The final weights are:\n', weights_f)
+
+            e2 = timer.time()
+            time_needed.append(e2 - s2)
 
             # Plot WL incrementor as a function of time
-            s1 = timer.time()
+            s3 = timer.time()
             plt.figure()  # ready to plot!
             plt.step(time, wl_incrementor)
             plt.xlabel('Time (ns)')
@@ -390,26 +450,26 @@ def main():
             plt.title('Wang-Landau incrementor as a function of time')
             plt.grid()
             plt.savefig('WL_t_%s.png' % args.keyword[i], dpi=600)
-            e1 = timer.time()
-            time_needed.append(e1 - s1)
+            e3 = timer.time()
+            time_needed.append(e3 - s3)
             plt.show()
 
             # Plot the equilbrated histogram
-            s2 = timer.time()
+            s4 = timer.time()
             plt.figure()
             plt.bar(np.arange(1, log_info.N_states + 1), height=equil_counts)
             plt.xlabel('States')
             plt.ylabel('Counts')
-            plt.title('The equilibrated histogram of the simulation')
+            plt.title('The equilibrated histogram of the simulation (%s ns)' % str(EXE.equil_time))
             if max(equil_counts) >= 10000:
                 plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
             plt.grid()
             plt.savefig('Equil_hist_%s.png' % args.keyword[i], dpi=600)
-            e2 = timer.time()
-            time_needed.append(e2 - s2)
+            e4 = timer.time()
+            time_needed.append(e4 - s4)
             plt.show()
 
-        s3 = timer.time()
+        s5 = timer.time()
         if log_info.fixed is True:
             print('This is a fixed-weight expanded ensemble simulation.')
             print('Accordingly, only the final histogram will be output and saved.')
@@ -431,9 +491,19 @@ def main():
             plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
         plt.grid()
         plt.savefig('Final_hist_%sns_%s.png' % (ftime_png, args.keyword[i]), dpi=600)
-        e3 = timer.time()
-        time_needed.append(e3 - s3)
+        e5 = timer.time()
+        time_needed.append(e5 - s5)
         plt.show()
-
-        print('Elasped time (including plotting): %s seconds.'
+        
+        s6 = timer.time()
+        # Average weights calculation
+        weights_a = EXE.get_avg_weights(args.log[i], args.avg_len)
+        print('The average weights of the last %s ns' % str(args.avg_len), 
+        'of the simulation before the weights are equilibrated (from %s to %s ns) are:\n' 
+        %(str(EXE.avg_start / 1000), str(EXE.avg_end / 1000) ), weights_a, '\n')
+        e6 = timer.time()
+        time_needed.append(e6 - s6)
+        
+    print('%s files analyzed.' % len(args.log))
+    print('Total time elapsed (including plotting): %s seconds.\n'
               % sum(time_needed))
