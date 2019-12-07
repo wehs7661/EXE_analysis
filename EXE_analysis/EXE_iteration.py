6a9db6ec6e56bc7dcdb51f91af509a39c94d942b
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2019-12-06 19:29:28 -0700

    Finished the development of the iteration method

--- EXE_analysis/EXE_iteration.py
+++ EXE_analysis/EXE_iteration.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 import numpy as np
+from sklearn import linear_model
 
 def initialize():
     parser = argparse.ArgumentParser(
@@ -17,12 +18,6 @@ def initialize():
                         '--log',
                         type=str,
                         help='The file name of the log file.')
-    parser.add_argument('-e',
-                        '--estimate',
-                        default=True,
-                        action='store_false',
-                        help='Whether to estimate the weights of the region\
-                             that N_k =0.')
     
     args_parse = parser.parse_args()
 
@@ -67,9 +62,10 @@ class WeightsIteration:
 
         final_found = False
         line_n = 0
-        count, G = np.zeros(self.N_states), np.zeros(self.N_states)
-        dG, ddG = np.zeros(self.N_states), np.zeros(self.N_states)
+        N, count = np.zeros(self.N_states), np.zeros(self.N_states)
+        G, dG = np.zeros(self.N_states), np.zeros(self.N_states)
 
+        # First read in the data needed
         for l in lines:  # direction: upward
             line_n += 1
             if 'dG(in kT)' in l:   # should find this line first 
@@ -83,6 +79,7 @@ class WeightsIteration:
                 for i in range(self.N_states):
                     # start from lines[line_n - 3]
                     # 'MC-lambda information' is lines[line_n - 1]
+                    N[i] = int(lines[line_n - 3 - i].split()[0])
                     count[i] = float(lines[line_n - 3 - i].split()[count_idx])
                     G[i] = float(lines[line_n - 3 - i].split()[G_idx])
                     dG[i] = float(lines[line_n - 3 - i].split()[dG_idx])
@@ -93,6 +90,40 @@ class WeightsIteration:
 
             if '  Step  ' in l and final_found is True:
                 break
+
+        # Start the weights iteration
+        G_adjstd = np.zeros(self.N_states)
+        n_zeros = 0     # number of 0 weights
+        n_adjstd = 0    # number of states that have been adjusted (including 0 weights)
+        model = linear_model.LinearRegression()
+        for i in range(state_coul_off):
+            if count[i] == 0:
+                break
+            if G[i] == 0:
+                n_zeros += 1
+                n_adjstd += 1
+                G_adjstd[i] == 0
+            else:
+                n_adjstd += 1
+                G_adjstd[i] = G[i] + np.log(count[i - 1] / count[i])
+
+        # estimate the weights of the state with 0 count
+        # In the region of scaling coulombic force, dG is approximately linear
+        dG_input = dG[n_zeros - 1:n_adjstd]    # data for linear regression
+        N_input = N[n_zeros - 1:n_adjstd]
+        results = model.fit(N_input.reshape(-1,1), dG_input)
+        n_prediction = state_coul_off - (len(dG_input) + n_zeros)
+
+        for i in range(n_prediction):
+            G_adjstd[n_adjstd + i] = G_adjstd[n_adjstd - 1 + i] + model.predict([[N_input[-1] + 1 + i]])
+
+        # The remaining weights will be the same as the original ones
+        for i in range(state_coul_off - 1, self.N_states):
+            G_adjstd[i] = G[i]
+
+        R2 = model.score(N_input.reshape(-1,1), dG_input)
+
+        return G_adjstd, R2
 
 def main():
     args = initialize()
@@ -105,5 +136,17 @@ def main():
             print('No log files provided or found! Please check if the directory is correct or specify the name of the log files.')
 
     WI = WeightsIteration(args.log)
-    WI.adjust_weights(args.log)
-    
+    G_new, R2 = WI.adjust_weights(args.log)
+
+    G_new_str = ''
+    for i in range(len(G_new)):
+        G_new_str += '%6.5f ' % G_new[i]
+
+    result_str = '\nData analysis of the file %s:' % args.log
+    print(result_str)
+    print('=' * len(result_str))
+    print('As a result of the weights iteration, the new weights for the next iteration are:')
+    print(G_new_str)
+    print('\nFor the region of scaling electrostatic interactions, if the count of a state is 0, we applied linear regression on dG to estimate the new weights.')
+    print('\nThe R-squared value of the linear regression is: %s' % R2)
+        
